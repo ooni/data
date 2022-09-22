e733875f00a066664e6f7ab48e3a417d6b8c082c
Arturo Filastò <arturo@filasto.net>
2022-09-22 12:53:07 +0200

    Set default probe_network_name

--- oonidata/dataformat.py
+++ oonidata/dataformat.py
@@ -82,7 +82,6 @@ class BaseMeasurement(BaseModel):
     test_start_time: str
 
     probe_asn: str
-    probe_network_name: Optional[str]
     probe_cc: str
     probe_ip: Optional[str]
 
@@ -99,6 +98,7 @@ class BaseMeasurement(BaseModel):
 
     test_keys: BaseTestKeys
     test_helpers: Optional[dict] = None
+    probe_network_name: Optional[str] = None
     data_format_version: Optional[str] = None
     measurement_uid: Optional[str] = None
 
