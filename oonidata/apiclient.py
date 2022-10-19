312132135aeddd9d92820cbac9134faf908f1bd7
Arturo Filastò <arturo@filasto.net>
2022-10-19 17:48:28 +0200

    Fix the remaining tests

--- oonidata/apiclient.py
+++ oonidata/apiclient.py
@@ -1,5 +1,6 @@
-import requests
 from typing import Optional
+import orjson
+import requests
 
 
 def get_raw_measurement(report_id: str, input: Optional[str] = None) -> bytes:
@@ -9,3 +10,14 @@ def get_raw_measurement(report_id: str, input: Optional[str] = None) -> bytes:
     r = requests.get("https://api.ooni.io/api/v1/measurement_meta", params=params)
     j = r.json()
     return j["raw_measurement"].encode("utf-8")
+
+
+def get_measurement_dict(report_id: str, input: Optional[str] = None) -> dict:
+    params = params = {"report_id": report_id, "full": True}
+    if input:
+        params["input"] = input
+    r = requests.get("https://api.ooni.io/api/v1/measurement_meta", params=params)
+    j = r.json()
+    msmt_dict = orjson.loads(j["raw_measurement"])
+    msmt_dict["measurement_uid"] = j["measurement_uid"]
+    return msmt_dict
--- oonidata/verdicts.py
+++ oonidata/verdicts.py
@@ -290,8 +290,7 @@ def make_dns_baseline(
     if isinstance(res, list) and len(res) > 0:
         dns_baseline.tls_consistent_answers = [row[0] for row in res]
 
-    q = """SELECT probe_cc, probe_asn, failure, answer
-    FROM obs_dns
+    q = """SELECT probe_cc, probe_asn, failure, answer FROM obs_dns
     WHERE domain_name = %(domain_name)s 
     AND timestamp >= %(start_day)s
     AND timestamp <= %(end_day)s
