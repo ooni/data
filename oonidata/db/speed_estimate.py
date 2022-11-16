fb6e6b094410e853d12e46ecbed905368332d29e
Arturo Filastò <arturo@filasto.net>
2022-11-16 20:29:22 +0100

    Fix

--- oonidata/db/speed_estimate.py
+++ oonidata/db/speed_estimate.py
@@ -12,12 +12,13 @@ def main():
     samples = []
     last_count = get_count(click_client)
     while True:
-
-        delta = get_count(click_client) - last_count
+        cur_count = get_count(click_client)
+        delta = cur_count - last_count
         samples.append(delta)
         print(f"average speed: {sum(samples)/len(samples)}")
         if len(samples) > 10:
             samples = samples[-10:]
+        last_count = cur_count
         time.sleep(1.0)
 
 
