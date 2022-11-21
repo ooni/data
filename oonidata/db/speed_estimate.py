e8a38de9aa0db8a2054895a1a4125c4449bde0f6
Arturo Filastò <arturo@filasto.net>
2022-11-22 00:42:47 +0100

    speed estimate

--- oonidata/db/speed_estimate.py
+++ oonidata/db/speed_estimate.py
@@ -8,14 +8,21 @@ def get_count(client) -> int:
 
 
 def main():
+    t0 = time.monotonic()
     click_client = Client.from_url("clickhouse://localhost/")
     samples = []
     last_count = get_count(click_client)
+    c0 = last_count
     while True:
         cur_count = get_count(click_client)
+
+        t_delta = time.monotonic() - t0
+        c_delta = cur_count - c0
+
         delta = cur_count - last_count
         samples.append(delta)
-        print(f"average speed: {sum(samples)/(len(samples)*2)}")
+        print(f"rolling avg: {sum(samples)/(len(samples)*2)}")
+        print(f"overall avg: {c_delta/t_delta}")
         if len(samples) > 10:
             samples = samples[-10:]
         last_count = cur_count
