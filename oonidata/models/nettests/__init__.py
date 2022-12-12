8d28f27ab21acacb1afb1c141478f732c5dd52df
Arturo Filastò <arturo@filasto.net>
2022-12-12 12:17:34 +0100

    typing.get_args doesn't work in python 3.7

--- oonidata/models/nettests/__init__.py
+++ oonidata/models/nettests/__init__.py
@@ -1,5 +1,4 @@
 from typing import Union
-import typing
 
 from .base_measurement import BaseMeasurement
 from .dnscheck import DNSCheck
@@ -8,10 +7,11 @@ from .tor import Tor
 from .web_connectivity import WebConnectivity
 from .whatsapp import Whatsapp
 
+SUPPORTED_CLASSES = [WebConnectivity, Tor, DNSCheck, Signal, Whatsapp, BaseMeasurement]
 SupportedDataformats = Union[
     WebConnectivity, Tor, DNSCheck, Signal, Whatsapp, BaseMeasurement
 ]
+
 NETTEST_MODELS = {
-    nettest_class.__test_name__: nettest_class
-    for nettest_class in typing.get_args(SupportedDataformats)
+    nettest_class.__test_name__: nettest_class for nettest_class in SUPPORTED_CLASSES
 }
