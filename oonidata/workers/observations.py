e5f6bd26e2ec6df11379490bee3e1ef6e5681794
Arturo Filastò <arturo@filasto.net>
2023-04-10 14:49:49 +0900

    Port observation generation to dask

--- oonidata/workers/observations.py
+++ oonidata/workers/observations.py
@@ -1,30 +1,24 @@
-import queue
 import pathlib
 import logging
-import traceback
 import dataclasses
-import multiprocessing as mp
-from multiprocessing.synchronize import Event as EventClass
-
-from threading import Thread
-from base64 import b32decode
 from datetime import date, timedelta
 
 from typing import (
-    Callable,
     List,
     Optional,
-    Union,
 )
 
 import statsd
+
+import dask
+from dask.distributed import Client, progress
+
 from oonidata.analysis.datasources import load_measurement
 from oonidata.datautils import PerfTimer
 
 from oonidata.netinfo import NetinfoDB
 
 from oonidata.dataclient import (
-    MeasurementListProgress,
     date_interval,
     iter_measurements,
 )
@@ -34,28 +28,35 @@ from oonidata.db.connections import (
 )
 from oonidata.transforms import measurement_to_observations
 from oonidata.workers.common import (
-    StatusMessage,
     get_prev_range,
     make_db_rows,
     maybe_delete_prev_range,
-    run_status_thread,
 )
 
 log = logging.getLogger("oonidata.processing")
 
 
+@dask.delayed
 def make_observation_in_day(
-    db: Union[ClickhouseConnection, CSVConnection],
-    netinfodb: NetinfoDB,
+    probe_cc: List[str],
+    test_name: List[str],
+    csv_dir: Optional[pathlib.Path],
+    clickhouse: Optional[str],
+    data_dir: pathlib.Path,
+    fast_fail: bool,
     day: date,
-    test_name=[],
-    probe_cc=[],
-    fast_fail=False,
-    progress_callback: Optional[Callable[[MeasurementListProgress], None]] = None,
 ):
-    bucket_date = day.strftime("%Y-%m-%d")
-
     statsd_client = statsd.StatsClient("localhost", 8125)
+    netinfodb = NetinfoDB(datadir=data_dir, download=False)
+
+    db = None
+    if clickhouse:
+        db = ClickhouseConnection(clickhouse, row_buffer_size=10_000)
+    elif csv_dir:
+        db = CSVConnection(csv_dir)
+    assert db, "no DB chosen"
+
+    bucket_date = day.strftime("%Y-%m-%d")
 
     prev_ranges = []
     if isinstance(db, ClickhouseConnection):
@@ -79,7 +80,6 @@ def make_observation_in_day(
             test_name=test_name,
             start_day=day,
             end_day=day + timedelta(days=1),
-            progress_callback=progress_callback,
         )
     ):
         msmt = None
@@ -119,107 +119,6 @@ def make_observation_in_day(
     db.close()
 
 
-class ObservationMakerWorker(mp.Process):
-    def __init__(
-        self,
-        status_queue: mp.JoinableQueue,
-        day_queue: mp.JoinableQueue,
-        shutdown_event: EventClass,
-        data_dir: pathlib.Path,
-        probe_cc: List[str],
-        test_name: List[str],
-        clickhouse: Optional[str],
-        csv_dir: Optional[pathlib.Path],
-        fast_fail: bool,
-        process_id: int,
-        log_level: int = logging.INFO,
-    ):
-        super().__init__(daemon=True)
-        assert clickhouse or csv_dir, "missing either clickhouse or csv_dir"
-
-        self.day_queue = day_queue
-        self.data_dir = data_dir
-        self.probe_cc = probe_cc
-        self.test_name = test_name
-        self.clickhouse = clickhouse
-        self.csv_dir = csv_dir
-        self.fast_fail = fast_fail
-        self.process_id = process_id
-
-        self.shutdown_event = shutdown_event
-
-        self.status_queue = status_queue
-
-        log.setLevel(log_level)
-
-    def run(self):
-        current_idx = 0
-        day_str = ""
-
-        assert self.clickhouse or self.csv_dir, "missing either clickhouse or csv_dir"
-
-        netinfodb = NetinfoDB(datadir=self.data_dir, download=False)
-
-        db = None
-        if self.clickhouse:
-            db = ClickhouseConnection(self.clickhouse, row_buffer_size=10_000)
-        elif self.csv_dir:
-            db = CSVConnection(self.csv_dir)
-        assert db
-
-        def progress_callback(p: MeasurementListProgress):
-            self.status_queue.put(
-                StatusMessage(
-                    src="observation_maker",
-                    progress=p,
-                    idx=current_idx,
-                    day_str=day_str,
-                )
-            )
-
-        while not self.shutdown_event.is_set():
-            try:
-                day = self.day_queue.get(block=True, timeout=0.1)
-            except queue.Empty:
-                continue
-            try:
-                make_observation_in_day(
-                    db=db,
-                    netinfodb=netinfodb,
-                    day=day,
-                    test_name=self.test_name,
-                    probe_cc=self.probe_cc,
-                    fast_fail=self.fast_fail,
-                    progress_callback=progress_callback,
-                )
-            except Exception as exc:
-                log.error(f"failed to process {day}", exc_info=True)
-                self.status_queue.put(
-                    StatusMessage(
-                        src="observation_maker",
-                        exception=exc,
-                        traceback=traceback.format_exc(),
-                    )
-                )
-            finally:
-                log.info(f"finished processing day {day_str}")
-                self.day_queue.task_done()
-
-        log.info("process is done")
-        try:
-            db.close()
-            log.info("database closed")
-        except Exception as exc:
-            log.error("failed to flush database", exc_info=True)
-            self.status_queue.put(
-                StatusMessage(
-                    src="observation_maker",
-                    exception=exc,
-                    traceback=traceback.format_exc(),
-                )
-            )
-
-
 def start_observation_maker(
     probe_cc: List[str],
     test_name: List[str],
@@ -232,59 +131,24 @@ def start_observation_maker(
     fast_fail: bool,
     log_level: int = logging.INFO,
 ):
-    shutdown_event = mp.Event()
-    worker_shutdown_event = mp.Event()
+    dask_client = Client(threads_per_worker=4, n_workers=parallelism)
 
-    status_queue = mp.JoinableQueue()
+    assert clickhouse or csv_dir, "missing either clickhouse or csv_dir"
 
-    status_thread = Thread(
-        target=run_status_thread, args=(status_queue, shutdown_event)
-    )
-    status_thread.start()
-
-    observation_workers = []
-    day_queue = mp.JoinableQueue()
-    for idx in range(parallelism):
-        worker = ObservationMakerWorker(
-            status_queue=status_queue,
-            day_queue=day_queue,
-            shutdown_event=worker_shutdown_event,
-            data_dir=data_dir,
+    task_list = []
+    for day in date_interval(start_day, end_day):
+        t = make_observation_in_day(
             probe_cc=probe_cc,
             test_name=test_name,
-            clickhouse=clickhouse,
             csv_dir=csv_dir,
+            clickhouse=clickhouse,
+            data_dir=data_dir,
             fast_fail=fast_fail,
-            log_level=log_level,
-            process_id=idx,
+            day=day,
         )
-        worker.start()
-        log.info(f"started worker {worker.pid}")
-        observation_workers.append(worker)
+        task_list.append(t)
 
-    for day in date_interval(start_day, end_day):
-        day_queue.put(day)
+    t = dask.persist(*task_list)
+    progress(t)
 
-    log.info("waiting for the day queue to finish")
-    day_queue.join()
-
-    # we first need to tell the workers to stop doing their work, so that they
-    # flush all their bodies to the archiver thread
-    log.info("sending shutdown event")
-    worker_shutdown_event.set()
-    status_queue.join()
-
-    log.info(f"waiting for observation workers to finish running")
-    for idx, p in enumerate(observation_workers):
-        log.info(f"waiting observation_maker {idx} to join")
-        p.join()
-        log.info(f"waiting observation_maker {idx} to close")
-        p.close()
-
-    # We are done, we can now tell everybody to go home
-    log.info("sending shutdown event")
-    shutdown_event.set()
-
-    # Shutdown the status thread
-    log.info("shutting down the status thread")
-    status_thread.join()
+    t.compute()
