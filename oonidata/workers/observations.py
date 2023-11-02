ff5840fbd98aeb65e1f652a16244769cad848488
Arturo Filastò <arturo@filasto.net>
2023-11-02 15:26:24 +0100

    Wrap all ClickhouseConnection with a context

--- oonidata/workers/observations.py
+++ oonidata/workers/observations.py
@@ -65,53 +65,54 @@ def make_observations_for_file_entry_batch(
 ):
     netinfodb = NetinfoDB(datadir=data_dir, download=False)
     tbatch = PerfTimer()
-    db = ClickhouseConnection(clickhouse, row_buffer_size=row_buffer_size)
-    statsd_client = statsd.StatsClient("localhost", 8125)
-    ccs = ccs_set(probe_cc)
-    idx = 0
-    for bucket_name, s3path, ext, fe_size in file_entry_batch:
-        log.info(f"processing file s3://{bucket_name}/{s3path}")
-        t = PerfTimer()
-        try:
-            for msmt_dict in stream_measurements(
-                bucket_name=bucket_name, s3path=s3path, ext=ext
-            ):
-                # Legacy cans don't allow us to pre-filter on the probe_cc, so
-                # we need to check for probe_cc consistency in here.
-                if ccs and msmt_dict["probe_cc"] not in ccs:
-                    continue
-                msmt = None
-                try:
-                    t = PerfTimer()
-                    msmt = load_measurement(msmt_dict)
-                    if not msmt.test_keys:
-                        log.error(
-                            f"measurement with empty test_keys: ({msmt.measurement_uid})",
-                            exc_info=True,
-                        )
+    with ClickhouseConnection(clickhouse, row_buffer_size=row_buffer_size) as db:
+        statsd_client = statsd.StatsClient("localhost", 8125)
+        ccs = ccs_set(probe_cc)
+        idx = 0
+        for bucket_name, s3path, ext, fe_size in file_entry_batch:
+            log.info(f"processing file s3://{bucket_name}/{s3path}")
+            t = PerfTimer()
+            try:
+                for msmt_dict in stream_measurements(
+                    bucket_name=bucket_name, s3path=s3path, ext=ext
+                ):
+                    # Legacy cans don't allow us to pre-filter on the probe_cc, so
+                    # we need to check for probe_cc consistency in here.
+                    if ccs and msmt_dict["probe_cc"] not in ccs:
                         continue
-                    write_observations_to_db(msmt, netinfodb, db, bucket_date)
-                    # following types ignored due to https://github.com/jsocol/pystatsd/issues/146
-                    statsd_client.timing("oonidata.make_observations.timed", t.ms, rate=0.1)  # type: ignore
-                    statsd_client.incr("oonidata.make_observations.msmt_count", rate=0.1)  # type: ignore
-                    idx += 1
-                except Exception as exc:
-                    msmt_str = msmt_dict.get("report_id", None)
-                    if msmt:
-                        msmt_str = msmt.measurement_uid
-                    log.error(f"failed at idx: {idx} ({msmt_str})", exc_info=True)
+                    msmt = None
+                    try:
+                        t = PerfTimer()
+                        msmt = load_measurement(msmt_dict)
+                        if not msmt.test_keys:
+                            log.error(
+                                f"measurement with empty test_keys: ({msmt.measurement_uid})",
+                                exc_info=True,
+                            )
+                            continue
+                        write_observations_to_db(msmt, netinfodb, db, bucket_date)
+                        # following types ignored due to https://github.com/jsocol/pystatsd/issues/146
+                        statsd_client.timing("oonidata.make_observations.timed", t.ms, rate=0.1)  # type: ignore
+                        statsd_client.incr("oonidata.make_observations.msmt_count", rate=0.1)  # type: ignore
+                        idx += 1
+                    except Exception as exc:
+                        msmt_str = msmt_dict.get("report_id", None)
+                        if msmt:
+                            msmt_str = msmt.measurement_uid
+                        log.error(f"failed at idx: {idx} ({msmt_str})", exc_info=True)
 
-                    if fast_fail:
-                        db.close()
-                        raise exc
-            log.info(f"done processing file s3://{bucket_name}/{s3path}")
-        except Exception as exc:
-            log.error(f"failed to stream measurements from s3://{bucket_name}/{s3path}")
-            log.error(exc)
-        statsd_client.timing("oonidata.dataclient.stream_file_entry.timed", t.ms, rate=0.1)  # type: ignore
-        statsd_client.gauge("oonidata.dataclient.file_entry.kb_per_sec.gauge", fe_size / 1024 / t.s, rate=0.1)  # type: ignore
-    statsd_client.timing("oonidata.dataclient.batch.timed", tbatch.ms)  # type: ignore
-    db.close()
+                        if fast_fail:
+                            db.close()
+                            raise exc
+                log.info(f"done processing file s3://{bucket_name}/{s3path}")
+            except Exception as exc:
+                log.error(
+                    f"failed to stream measurements from s3://{bucket_name}/{s3path}"
+                )
+                log.error(exc)
+            statsd_client.timing("oonidata.dataclient.stream_file_entry.timed", t.ms, rate=0.1)  # type: ignore
+            statsd_client.gauge("oonidata.dataclient.file_entry.kb_per_sec.gauge", fe_size / 1024 / t.s, rate=0.1)  # type: ignore
+        statsd_client.timing("oonidata.dataclient.batch.timed", tbatch.ms)  # type: ignore
     return idx
 
 
@@ -125,24 +126,24 @@ def make_observation_in_day(
     day: date,
 ):
     statsd_client = statsd.StatsClient("localhost", 8125)
-    db = ClickhouseConnection(clickhouse, row_buffer_size=10_000)
 
     bucket_date = day.strftime("%Y-%m-%d")
 
-    prev_ranges = []
-    for table_name in ["obs_web"]:
-        prev_ranges.append(
-            (
-                table_name,
-                get_prev_range(
-                    db=db,
-                    table_name=table_name,
-                    bucket_date=bucket_date,
-                    test_name=test_name,
-                    probe_cc=probe_cc,
-                ),
+    with ClickhouseConnection(clickhouse, row_buffer_size=10_000) as db:
+        prev_ranges = []
+        for table_name in ["obs_web"]:
+            prev_ranges.append(
+                (
+                    table_name,
+                    get_prev_range(
+                        db=db,
+                        table_name=table_name,
+                        bucket_date=bucket_date,
+                        test_name=test_name,
+                        probe_cc=probe_cc,
+                    ),
+                )
             )
-        )
 
     t = PerfTimer()
     total_t = PerfTimer()
@@ -184,10 +185,10 @@ def make_observation_in_day(
     statsd_client.timing("oonidata.dataclient.daily.timed", total_t.ms)
 
     if len(prev_ranges) > 0:
-        for table_name, pr in prev_ranges:
-            maybe_delete_prev_range(db=db, prev_range=pr, table_name=table_name)
+        with ClickhouseConnection(clickhouse, row_buffer_size=10_000) as db:
+            for table_name, pr in prev_ranges:
+                maybe_delete_prev_range(db=db, prev_range=pr, table_name=table_name)
 
-    db.close()
     return total_size, total_msmt_count
 
 
@@ -207,7 +208,6 @@ def start_observation_maker(
         n_workers=parallelism,
     )
 
-    db = ClickhouseConnection(clickhouse)
     t_total = PerfTimer()
     total_size, total_msmt_count = 0, 0
     day_list = list(date_interval(start_day, end_day))
@@ -230,19 +230,20 @@ def start_observation_maker(
         log.info(
             f"finished processing {day} speed: {mb_per_sec}MB/s ({msmt_per_sec}msmt/s)"
         )
-        db.execute(
-            "INSERT INTO oonidata_processing_logs (key, timestamp, runtime_ms, bytes, msmt_count, comment) VALUES",
-            [
+        with ClickhouseConnection(clickhouse) as db:
+            db.execute(
+                "INSERT INTO oonidata_processing_logs (key, timestamp, runtime_ms, bytes, msmt_count, comment) VALUES",
                 [
-                    "oonidata.bucket_processed",
-                    datetime.utcnow(),
-                    int(t.ms),
-                    size,
-                    msmt_count,
-                    day.strftime("%Y-%m-%d"),
-                ]
-            ],
-        )
+                    [
+                        "oonidata.bucket_processed",
+                        datetime.utcnow(),
+                        int(t.ms),
+                        size,
+                        msmt_count,
+                        day.strftime("%Y-%m-%d"),
+                    ]
+                ],
+            )
 
     mb_per_sec = round(total_size / t_total.s / 10**6, 1)
     msmt_per_sec = round(total_msmt_count / t_total.s)
