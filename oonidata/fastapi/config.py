a87250e05d14a3212c44fd04368bf3e5ffd68f46
Arturo Filastò <arturo@filasto.net>
2023-11-21 19:32:58 +0100

    Add support for filtering measurements and data format returned value

--- oonidata/fastapi/config.py
+++ oonidata/fastapi/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "OONI Data API"
+    base_url: str = "https://api.ooni.io"
     clickhouse_url: str = "clickhouse://localhost"
 
 
