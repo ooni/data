3fd741391ca5c0e04a7bb15adca850a01c852184
Arturo Filastò <arturo@filasto.net>
2024-09-02 16:01:59 -0400

    Implement observable framework dataviz for observation by failure

---
+++ oonipipeline/src/oonipipeline/dashboard/src/lib/observations.js
@@ -0,0 +1,18 @@
+async function json(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
+  return await response.json();
+}
+
+export async function getObservationsAggregate(params) {
+  const query = Object.keys(params)
+    .map((key) => {
+      if (Array.isArray(params[key])) {
+        return params[key].map((value) => `${key}=${value}`).join("&");
+      }
+      return `${key}=${params[key]}`;
+    })
+    .join("&");
+  console.log(`running query ${query}`);
+  return json(`http://localhost:8000/api/v2/observations-aggregate?${query}`);
+}
