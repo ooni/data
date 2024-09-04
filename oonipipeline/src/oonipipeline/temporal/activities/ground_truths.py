01f3b80f07cd221a4a9065508eb1f7ba5fad0c61
Arturo Filastò <arturo@filasto.net>
2024-09-04 12:27:23 -0400

    Fix logic bug in ground truth building

--- oonipipeline/src/oonipipeline/temporal/activities/ground_truths.py
+++ oonipipeline/src/oonipipeline/temporal/activities/ground_truths.py
@@ -42,9 +42,9 @@ def make_ground_truths_in_day(params: MakeGroundTruthsParams):
 
     dst_path = get_ground_truth_db_path(data_dir=params.data_dir, day=params.day)
 
-    if dst_path.exists() or params.force_rebuild:
+    if dst_path.exists() and params.force_rebuild:
         dst_path.unlink()
-    else:
+    elif dst_path.exists():
         return
 
     t = PerfTimer()
