077e234bd258c359592f8e3133205ec356323246
Aaron Gibson <aagbsn@extc.org>
2026-03-24 14:55:49 +0100

    add fastpath table to init

--- tests/integration/clickhouse_init.sql
+++ tests/integration/clickhouse_init.sql
@@ -31,3 +31,52 @@ CREATE TABLE ooni.fingerprints_http
 )
 ENGINE = EmbeddedRocksDB
 PRIMARY KEY name;
+
+CREATE TABLE ooni.fastpath
+(
+    `measurement_uid` String,
+    `report_id` String,
+    `input` String,
+    `probe_cc` LowCardinality(String),
+    `probe_asn` Int32,
+    `test_name` LowCardinality(String),
+    `test_start_time` DateTime,
+    `measurement_start_time` DateTime,
+    `filename` String,
+    `scores` String,
+    `platform` String,
+    `anomaly` String,
+    `confirmed` String,
+    `msm_failure` String,
+    `domain` String,
+    `software_name` String,
+    `software_version` String,
+    `control_failure` String,
+    `blocking_general` Float32,
+    `is_ssl_expected` Int8,
+    `page_len` Int32,
+    `page_len_ratio` Float32,
+    `server_cc` String,
+    `server_asn` Int8,
+    `server_as_name` String,
+    `update_time` DateTime64(3) MATERIALIZED now64(),
+    `test_version` String,
+    `architecture` String,
+    `engine_name` LowCardinality(String),
+    `engine_version` String,
+    `test_runtime` Float32,
+    `blocking_type` String,
+    `test_helper_address` LowCardinality(String),
+    `test_helper_type` LowCardinality(String),
+    `ooni_run_link_id` Nullable(UInt64),
+    `is_verified` Int8 DEFAULT 0,
+    `nym` String DEFAULT '',
+    `zkp_request` String DEFAULT '',
+    `age_range` String DEFAULT '',
+    `msm_range` String DEFAULT '',
+    INDEX fastpath_rid_idx report_id TYPE minmax GRANULARITY 1,
+    INDEX measurement_uid_idx measurement_uid TYPE minmax GRANULARITY 8
+)
+ENGINE = ReplicatedReplacingMergeTree('/clickhouse/{cluster}/tables/ooni/fastpath/{shard}', '{replica}', update_time)
+ORDER BY (measurement_start_time, report_id, input, measurement_uid)
+SETTINGS index_granularity = 8192;
