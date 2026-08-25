d4d65998d2e7b21840177e28c40a559f6d3e1cdc
Luis Díaz <ldiazn98@gmail.com>
2026-08-25 16:51:34 +0200

    Starting to implement cusum detection on new series

--- oonipipeline/src/oonipipeline/analysis/detectorV2.py
+++ oonipipeline/src/oonipipeline/analysis/detectorV2.py
@@ -1,10 +1,15 @@
+from mypy.state import state
+from enum import StrEnum
+import logging
+import math
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Mapping
-from .rules import get_layer, get_rule, Evidence, OutcomeClass
+
 from clickhouse_driver import Client as ClickhouseClient
-import logging
+
+from .rules import Evidence, OutcomeClass, get_layer, get_rule
 
 log = logging.getLogger(__name__)
 
@@ -22,23 +27,27 @@ class Cell:
     tcp_rule_counts: Mapping[str, int]  # sumMap([top_tcp_rule_id], [toUInt32(1)])
     tls_rule_counts: Mapping[str, int]  # sumMap([top_tls_rule_id], [toUInt32(1)])
     # layer -> amount of blocked measurements in that layer
-    k_blocked: Mapping[str, int] # layer -> |Blocked measurements|
-    n_ok: Mapping[str, int] # layer -> |Ok measurements|
-    discarded: Mapping[str, int] # layer -> |Ignored rules|
+    k_blocked: Mapping[str, int]  # layer -> |Blocked measurements|
+    n_ok: Mapping[str, int]  # layer -> |Ok measurements|
+    discarded: Mapping[str, int]  # layer -> |Ignored rules|
 
-    # def next_s_pos(self, s_prev: float, ) -> float:
-    #     # s_pos = max(0.0, s_pos + sum(c * log_lr[rule_id] for rule_id, c in counts.items()))
-    #     return max(0, s_prev + ())
+class State(StrEnum):
+
+    UNKNOWN = 'UNK'
+    OK = 'OK'
+    BLOCK = 'BLOCK'
 
 @dataclass
-class ChangePoints:
+class ChangePoint:
     domain: str
     probe_cc: str
     probe_asn: str
     resolver_asn: str
-    ts: datetime
-    s: float
+    ts_hour: datetime
+    s_neg: float
+    s_pos: float
     h: float
+    state: State # Acquired in this hour
 
 
 def get_cells(
@@ -73,11 +82,7 @@ def get_cells(
     GROUP BY domain, probe_cc, probe_asn, resolver_asn, ts_hour;
     """
 
-    params = {
-        "domains": domains,
-        "start_time": start_time,
-        "end_time": end_time
-    }
+    params = {"domains": domains, "start_time": start_time, "end_time": end_time}
     if probe_cc:
         params["probe_cc"] = probe_cc
 
@@ -91,19 +96,18 @@ def get_cells(
 
     rule_maps = ["dns_rule_counts", "tcp_rule_counts", "tls_rule_counts"]
     for row in rows:
-        row['k_blocked'] = defaultdict(int)
-        row['n_ok'] = defaultdict(int)
-        row['discarded'] = defaultdict(int)
+        row["k_blocked"] = defaultdict(int)
+        row["n_ok"] = defaultdict(int)
+        row["discarded"] = defaultdict(int)
         for rule_map in rule_maps:
-
             if not row.get(rule_map):
                 continue
 
             rule_ids, counts = row[rule_map]
             # convert mapping to a format easier to use
-            row[rule_map] = dict(zip(rule_ids, counts)) # TODO not sure if useful
+            row[rule_map] = dict(zip(rule_ids, counts))  # TODO not sure if useful
 
-            for (rule_id, count) in row[rule_map].items():
+            for rule_id, count in row[rule_map].items():
                 try:
                     rule = get_rule(rule_id)
                 except ValueError as e:
@@ -113,33 +117,118 @@ def get_cells(
                 # See: https://docs.ooni.org/data/pipeline-implementation-plan ,
                 # section 3.6
                 layer = get_layer(rule_id)
-                if rule.evidence == Evidence.SCORED and rule.outcome_class == OutcomeClass.BLOCKED:
-                    row['k_blocked'][layer] += count
+                if (
+                    rule.evidence == Evidence.SCORED
+                    and rule.outcome_class == OutcomeClass.BLOCKED
+                ):
+                    row["k_blocked"][layer] += count
                 elif rule.outcome_class == OutcomeClass.OK:
-                    row['n_ok'][layer] += count
+                    row["n_ok"][layer] += count
                 else:
-                    row['discarded'][layer] += count
+                    row["discarded"][layer] += count
 
     return [Cell(**row) for row in rows]
 
 
 # TODO warmup and run it for every relevant (domain,probe_cc,probe_asn, resolver_asn)
-def compute_changepoints(
-    series: list[Cell], p_block: float = 0.1, p_ok: float = 0.9, edd: int = 1
-) -> list[ChangePoints]:
+class Detector:
+    def __init__(self):
+        self.s_pos = self.s_neg = 0
+        self.state = State.UNKNOWN  # UNK | OK | BLOCK
 
 
+    def compute_changepoints(
+        self,
+        series: list[Cell],
+        layer: str,
+        p0: float = 0.1,
+        p1: float = 0.9,
+        # TODO choose a better value for h depending on labeled corpus
+        # (implementation plan section 3.8)
+        h: float = 0.75,
+        warmup: bool = False,
+    ) -> list[ChangePoint]:
+        """
+        Assumes the input list has the following properties:
+            - Sorted by time, one entry per hour
+            - All cells are from the same (probe_cc, probe_asn, resolver_asn, domain)
 
-    return []
+        TODO: Definition of p1 and p0 is unclear
 
+        "warmup" runs the detector without generating any new changepoint, it
+        only updates internal state
+        """
+        results = []
+        w_block = math.log(p1 / p0)
+        w_clear = math.log((1.0 - p1) / (1.0 - p0))
+
+        for cell in series:
+            if (cp := self.step(cell, w_block, w_clear, layer, h)) and not warmup:
+                results.append(cp)
+
+        return results
+
+    def step(self, cell : Cell, w_block : float, w_clear : float, layer : str, h : float) -> ChangePoint | None:
+        # original state is unknown, run both series in parallel to discover
+        # current state
+        k = cell.k_blocked[layer]
+        n = cell.n_ok[layer]
+        llr = self.s_pos + k * w_block + (n - k) * w_clear
+
+        def make_cp(state: State) -> ChangePoint:
+            return ChangePoint(
+                domain = cell.domain,
+                probe_cc = cell.probe_cc,
+                probe_asn = cell.probe_asn,
+                resolver_asn = cell.resolver_asn,
+                s_neg = self.s_neg,
+                s_pos = self.s_pos,
+                state = state,
+                h = h,
+                ts_hour = cell.ts_hour
+            )
+
+        if self.state == State.UNKNOWN:
+            # s_pos = max(0.0, s_pos + k * w_blocked + (n - k) * w_clear)
+            self.s_pos = max(0, self.s_pos + llr)
+            self.s_neg = max(0, self.s_neg - llr)
+
+            if self.s_pos > h:
+                self.state = State.BLOCK
+                self.s_pos = self.s_neg = 0
+            elif self.s_neg > h:
+                self.state = State.OK
+                self.s_pos = self.s_neg = 0
+            # Don't return a changepoint: this is the initial state
+        elif self.state == State.BLOCK:
+            # Run s_neg accumulator: we wan't to see if the blocking signal
+            # goes down
+            self.s_neg = max(0, self.s_neg - llr)
+            if self.s_neg > h:
+                cp = make_cp(State.OK)
+                self.state = State.OK
+                self.s_pos = self.s_neg = 0
+                return cp
+        elif self.state == State.OK:
+            # Run s_pos accumulator: we wan't to see if the blocking signal
+            # goes up
+            self.s_pos = max(0, self.s_pos + llr)
+            if self.s_pos > h:
+                cp = make_cp(State.BLOCK)
+                self.state = State.BLOCK
+                self.s_neg = self.s_pos = 0
+                return cp
+
+        # TODO: Reset to unknown after long periouds without data
+        return None
 
 # Charts
 LAYERS = ["dns", "tcp", "tls"]
 
 OUTCOME_COLORS = {
-    "blocked": "#d62728",   # red
-    "ok": "#2ca02c",        # green
-    "discarded": "#7f7f7f", # gray
+    "blocked": "#d62728",  # red
+    "ok": "#2ca02c",  # green
+    "discarded": "#7f7f7f",  # gray
 }
 
 
@@ -196,15 +285,19 @@ def make_cells_histogram_chart(cells: list[Cell]):
                 width=900,
                 height=150,
                 title=alt.TitleParams(
-                    text=layer, fontSize=11, fontWeight="normal", color="gray",
-                    anchor="start", dy=-4, offset=2,
+                    text=layer,
+                    fontSize=11,
+                    fontWeight="normal",
+                    color="gray",
+                    anchor="start",
+                    dy=-4,
+                    offset=2,
                 ),
             )
         )
 
     charts = [
-        make_layer_chart(layer, show_x_axis=(layer == LAYERS[-1]))
-        for layer in LAYERS
+        make_layer_chart(layer, show_x_axis=(layer == LAYERS[-1])) for layer in LAYERS
     ]
     return (
         alt.vconcat(*charts, spacing=10)
@@ -261,9 +354,9 @@ def make_rule_histogram_chart(cells: list[Cell]):
         for rule_id, count in getattr(cell, field).items()
     ]
     df = pd.DataFrame(records)
-    df = df.groupby(
-        ["ts_hour", "layer", "rule_id", "outcome"], as_index=False
-    )["count"].sum()
+    df = df.groupby(["ts_hour", "layer", "rule_id", "outcome"], as_index=False)[
+        "count"
+    ].sum()
 
     def make_layer_chart(layer: str, show_x_axis: bool):
         return (
@@ -296,15 +389,19 @@ def make_rule_histogram_chart(cells: list[Cell]):
                 width=900,
                 height=150,
                 title=alt.TitleParams(
-                    text=layer, fontSize=11, fontWeight="normal", color="gray",
-                    anchor="start", dy=-4, offset=2,
+                    text=layer,
+                    fontSize=11,
+                    fontWeight="normal",
+                    color="gray",
+                    anchor="start",
+                    dy=-4,
+                    offset=2,
                 ),
             )
         )
 
     charts = [
-        make_layer_chart(layer, show_x_axis=(layer == LAYERS[-1]))
-        for layer in LAYERS
+        make_layer_chart(layer, show_x_axis=(layer == LAYERS[-1])) for layer in LAYERS
     ]
     return (
         alt.vconcat(*charts, spacing=10)
